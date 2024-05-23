00d6171bd7cffe972239ab11f536e3d60ccea8c2
niicolasaps <nicolasalmeida.ps@gmail.com>
2024-05-23 00:57:27 -0300

    feat: iphone e nokia instalacao de aplicativos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando o Nokia:");
+            Smartphone nokia = new Nokia("123456789", "Nokia 3310", "111111111111111", 16);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Testando o iPhone:");
+            Smartphone iphone = new Iphone("987654321", "iPhone 12", "222222222222222", 64);
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
